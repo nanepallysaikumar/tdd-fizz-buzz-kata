74c900c358330440efb591aa7af0f0c7f285ed76
Sai Kumar <prasanna.kandregula.lpk@gmail.com>
2022-12-08 20:05:11 +0530

    Feat: Added number 2 to display on the screen.

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ function App() {
   return (
     <div className="App">
       <p data-testid="text-1">1</p>
+      <p data-testid="text-2">2</p>
     </div>
   );
 }
--- src/App.test.js
+++ src/App.test.js
@@ -5,3 +5,8 @@ test("it should render number 1 on first position", () => {
   render(<App />);
   expect(screen.getByTestId("text-1").textContent).toEqual("1");
 });
+
+test("it should render number 2 on second position", () => {
+  render(<App />);
+  expect(screen.getByTestId("text-2").textContent).toEqual("2");
+});
